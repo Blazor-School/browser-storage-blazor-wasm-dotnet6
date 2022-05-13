5402e02284aaeae9a705e6e66c35f587bcae449e
Red Wei <hevilhuy2@gmail.com>
2022-05-13 17:52:57 +0700

    Added session storage example.

---
+++ SessionStorage/Utils/SessionStorageAccessor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.JSInterop;
+
+namespace SessionStorage.Utils;
+
+public class SessionStorageAccessor
+{
+    private Lazy<IJSObjectReference> _accessorJsRef = new();
+    private readonly IJSRuntime _jsRuntime;
+
+    public SessionStorageAccessor(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task<T> GetValueAsync<T>(string key)
+    {
+        await WaitForReference();
+        var result = await _accessorJsRef.Value.InvokeAsync<T>("get", key);
+
+        return result;
+    }
+
+    public async Task SetValueAsync<T>(string key, T value)
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("set", key, value);
+    }
+
+    public async Task Clear()
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("clear");
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("remove", key);
+    }
+
+    private async Task WaitForReference()
+    {
+        if (_accessorJsRef.IsValueCreated is false)
+        {
+            _accessorJsRef = new(await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "/js/SessionStorageAccessor.js"));
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_accessorJsRef.IsValueCreated)
+        {
+            await _accessorJsRef.Value.DisposeAsync();
+        }
+    }
+}
---
+++ SessionStorage/wwwroot/js/SessionStorageAccessor.js
@@ -0,0 +1,19 @@
+﻿export function get(key)
+{
+    return window.sessionStorage.getItem(key);
+}
+
+export function set(key, value)
+{
+    window.sessionStorage.setItem(key, value);
+}
+
+export function clear()
+{
+    window.sessionStorage.clear();
+}
+
+export function remove(key)
+{
+    window.sessionStorage.removeItem(key);
+}
