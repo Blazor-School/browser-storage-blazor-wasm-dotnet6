ff15ac80c0347937a08803e872e0175bd240f7ca
Red Wei <hevilhuy2@gmail.com>
2022-05-13 17:42:26 +0700

    Added example for local storage.

--- LocalStorage/Program.cs
+++ LocalStorage/Program.cs
@@ -1,4 +1,5 @@
 using LocalStorage;
+using LocalStorage.Utils;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<LocalStorageAccessor>();
 
 await builder.Build().RunAsync();
